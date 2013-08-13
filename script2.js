4770dfe0a04ffa185e33751169d45033317cb2e9
brouxhaha <andy.debroux@gmail.com>
2013-08-13 12:07:17 -0700

    fixed last question not replacing third question

--- script2.js
+++ script2.js
@@ -1,14 +1,32 @@
-var allQuestions = [
-	{question: "Who was the first president of the USA?", choices: ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "George Jefferson"], correctAnswer: 1},
-	{question: "Who was the sixteenth president of the USA?", choices: ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "George Jefferson"], correctAnswer: 0},
-	{question: "Who was the third president of the USA?", choices: ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "George Jefferson"], correctAnswer: 2},
-	{question: "Who was the main character of <i>The Jeffersons</i>?", choices: ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "George Jefferson"], correctAnswer: 3}
-],
-		userScore = 0,
+//var quizInfo = JSON.parse("quizInfo.json"),
+var jsonObject = [
+	{
+		"question": "Who was the first president of the USA?", 
+		"choices": ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "George Jefferson"],
+		"correctAnswer": 1
+	},
+	{
+		"question": "Who was the sixteenth president of the USA?", 
+		"choices": ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "George Jefferson"], 
+		"correctAnswer": 0
+	},
+	{
+		"question": "Who was the third president of the USA?", 
+		"choices": ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "George Jefferson"], 
+		"correctAnswer": 2
+	},
+	{
+		"question": "Who was the main character of <i>The Jeffersons</i>?", 
+		"choices": ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "George Jefferson"], 
+		"correctAnswer": 3
+	}
+];
+var quizInfo = JSON.parse(jsonObject);
+var	userScore = 0,
 		displayQuestion = document.getElementsByClassName('question')[0],
 		displayAnswers = document.getElementsByTagName('li'),
 		questionNumber = 0,
-		numberQuestions = allQuestions.length,
+		numberQuestions = quizInfo.length,
 		numberAnswers = 0,
 		selectedAnswer,
 		nextQuestionButton = document.getElementsByClassName('next-question')[0];
@@ -19,16 +37,16 @@ nextQuestionButton.addEventListener('click', compareAnswers);
 
 function setQuestion(){
 	//console.log("set question: " + questionNumber);
-	displayQuestion.innerHTML = allQuestions[questionNumber]['question'];
-	numberAnswers = allQuestions[questionNumber]['choices'].length;
+	displayQuestion.innerHTML = quizInfo[questionNumber]['question'];
+	numberAnswers = quizInfo[questionNumber]['choices'].length;
 	for(var i = 0; i < numberAnswers; i++){
-		displayAnswers[i].getElementsByTagName('label')[0].innerHTML = allQuestions[questionNumber]['choices'][i];
+		displayAnswers[i].getElementsByTagName('label')[0].innerHTML = quizInfo[questionNumber]['choices'][i];
 		displayAnswers[i].getElementsByTagName('input')[0].checked = false;
 	}
 }
 
 function compareAnswers(){
-	var correctAnswer = allQuestions[questionNumber]['correctAnswer'];
+	var correctAnswer = quizInfo[questionNumber]['correctAnswer'];
 	for(var j = 0; j < numberAnswers; j++){
 		if(displayAnswers[j].getElementsByTagName('input')[0].checked){
 			selectedAnswer = displayAnswers[j].getElementsByTagName('input')[0];
